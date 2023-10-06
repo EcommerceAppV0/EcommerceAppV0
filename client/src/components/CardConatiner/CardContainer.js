0bc38c5664326d3d8a8a37dd4fa0029bc52b18aa
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-10-07 01:27:56 +0200

    gg marar o5ra

--- client/src/components/CardConatiner/CardContainer.js
+++ client/src/components/CardConatiner/CardContainer.js
@@ -1,25 +1,46 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
-const CardContainer = ({ ispromo, prod }) => {
+import { useUpdateListsMutation } from '../../slicers/userApiSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser } from "../../slicers/userSlicer"
+const CardContainer = ({ prod }) => {
+    const { user } = useSelector((state) => state.value)
     const [click, setClicked] = useState(true)
     const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const [updateLists] = useUpdateListsMutation()
     const rates = new Array(+prod.rate).fill("rate")
     const greys = new Array(5 - rates.length).fill("rate")
+    const handeyeClick = async () => {
+        try {
+            if (!user.wishlist.includes(prod)) {
+                const res = await updateLists({ wishlist: [...user.wishlist, { ...prod, inWishList: true }], id: user.userId }, user.userId).unwrap()
+                dispatch(setUser({ ...user, wishlist: [...user.wishlist, prod] }))
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <div className='card-container'>
             <div className='card-images'>
                 <div className='promtion-div-wish'>
                     <button style={{ backgroundColor: prod.status === "new" ? "#0F6" : "#DB4444", visibility: prod.status === "new" || prod.status === "sale" ? "block" : "hidden" }}>{prod.status === "new" ? "New" : `-${prod.promo}%`}</button>
                     <i
-                        onClick={() =>
-                            // then Changed
-                            setClicked(!click)
+                        onClick={() => {
+                            //  toaost and
+                            setClicked(!click);
+                            handeyeClick()
+                        }
                         }
                         className={click ? "fa-regular fa-heart fa-lg" : "fa-solid fa-heart fa-lg"} style={{ color: "#DB4444" }}></i>
                 </div>
                 <i className="fa-regular fa-eye fa-lg"
-                    onClick={() => navigate('/productdetails')}
+                    onClick={() => {
+
+
+
+                    }}
                     style={{ color: "black" }}></i>
 
                 <img src={prod.images[0]} alt='product' />

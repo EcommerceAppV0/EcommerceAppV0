f2a652583a70e9d71fba946705e7d2819a2c8cd3
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-10-06 22:50:18 +0200

    fixed some Console.log Warning

--- client/src/components/CategoryDiv/CategoryDiv.js
+++ client/src/components/CategoryDiv/CategoryDiv.js
@@ -2,7 +2,6 @@ import React from "react";
 
 function CategoryDiv({category}) {
     
-console.log("hi");
   return (
     <div className="category-div-sub">
       <div className="d-flex gap-3 align-items-center flex-column">

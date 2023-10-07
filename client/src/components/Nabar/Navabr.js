728497f1f7ab88031e8ed38e4eb98bb219ab8fb5
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-10-07 18:01:32 +0200

    fixed a Lot of toasts

--- client/src/components/Nabar/Navabr.js
+++ client/src/components/Nabar/Navabr.js
@@ -6,6 +6,8 @@ import "./style.css"
 import { useSelector, useDispatch } from 'react-redux'
 import { logout } from '../../slicers/userSlicer'
 import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify"
+
 
 const Navabr = () => {
     const { loggedIn } = useSelector((state) => state.value)
@@ -38,7 +40,16 @@ const Navabr = () => {
                         onClick={() => navigate("/about")}
                     >About</div>
                     <div className='link'
-                        onClick={() => navigate("/signup")}
+                        onClick={() => !loggedIn ? navigate("/signup") : toast.info(`You are signed in Logout First`, {
+                            position: "top-center",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: false,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "dark",
+                        })}
                     >Sign Up</div>
                 </div>
                 <div className='frame'>
@@ -85,7 +96,7 @@ const Navabr = () => {
                                     </svg>
                                     Manage My Account
                                 </Dropdown.Item>
-                                <Dropdown.Item>
+                                <Dropdown.Item onClick={() => navigate("/cart")}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="22" viewBox="0 0 20 22" fill="none">
                                         <path d="M1 5.3V19.5C1 19.7652 1.10536 20.0196 1.29289 20.2071C1.48043 20.3946 1.73478 20.5 2 20.5H18C18.2652 20.5 18.5196 20.3946 18.7071 20.2071C18.8946 20.0196 19 19.7652 19 19.5V5.3H1Z" stroke="#FAFAFA" strokeWidth="1.5" strokeLinejoin="round" />
                                         <path d="M19 5.3L16.1665 1.5H3.8335L1 5.3M13.7775 8.6C13.7775 10.699 12.0865 12.4 10 12.4C7.9135 12.4 6.222 10.699 6.222 8.6" stroke="#FAFAFA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
--- client/src/components/WishList/WishList.js
+++ client/src/components/WishList/WishList.js
@@ -1,29 +1,80 @@
 //hakim lol
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css";
-// import deleted from "../../assets/images/delete.svg"
-// import Cart1 from "../../assets/images/Cart1.svg"
-// import inspector from "../../assets/images/inspect.svg"
-// import pc from "../../assets/images/pc.png"
-// import manetteka7la from "../../assets/images/manetteka7la.png"
-// import manettehamra from "../../assets/images/manettehamra.png"
-// import jacket from "../../assets/images/jacket.png"
-// import ecran from "../../assets/images/ecran.png"
-// import cartegraphique from "../../assets/images/cartegraphique.png"
-// import sacgucci from "../../assets/images/sacgucci.png"
-// import clavier from "../../assets/images/clavier.png"
-// import najma from "../../assets/images/najma.svg"
 import CardContainer from '../CardConatiner/CardContainer';
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { useUpdateListsMutation } from '../../slicers/userApiSlice'
+import { setUser } from '../../slicers/userSlicer';
+import { toast } from "react-toastify"
 
 const Wishlist = () => {
+  const dispatch = useDispatch()
+  const [updateLists] = useUpdateListsMutation()
   const { user } = useSelector((state) => state.value)
+  const { products } = useSelector((state) => state.prod)
+  const [limit, setLimit] = useState(4)
+  const handeyeClickaddAll = async () => {
+    try {
+      if (user.cartlist.length !== user.wishlist.length) {
+        const res = await updateLists({ cartlist: [...user.wishlist], id: user.userId }).unwrap()
+        dispatch(setUser({ ...user, cartlist: [...user.wishlist] }))
+        toast.success(` All Added`, {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      } else {
+        toast.warning(`Already Added`, {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <div className='all-wishList'>
-      {user.wishlist.map((prod) => <CardContainer key={prod.id} prod={prod} />)}
+    <div className='all-wishList-container d-flex'>
+      <div className='d-flex justify-content-between align-items-center'>
+        <div className='d-flex gap-3 wishlist-text'>
+          <span>Wishlist</span>
+          ({user.wishlist.length})
+        </div>
+        <button className='move-to-bag'
+          onClick={() => handeyeClickaddAll()}
+        >Move All To Cart</button>
+      </div>
+      <div className='all-wishList'>
+        {!user.wishlist.length ? <h1>No Items In The WishList Go Add Some</h1> : user.wishlist.map((prod) => <CardContainer key={prod.id} prod={prod} />)}
+      </div>
+      <div className='just-for-you d-flex justify-content-between align-items-center'>
+        <div className='d-flex gap-3 wishlist-text align-items-center'>
+          <div className='red-small-div'></div>
+          <span>Just For You</span>
+        </div>
+        <button className='move-to-bag'
+          onClick={() => limit === 4 ? setLimit(products.length) : setLimit(4)}
+        >{limit === 4 ? "See All" : "See Less"}</button>
+      </div>
+      <div className='all-wishList'>
+        {products.map((prod, i) => {
+          if (i < limit) {
+            return <CardContainer key={prod.id} prod={prod} />
+          }
+        })}
+      </div>
     </div>
-
   );
 };
 
